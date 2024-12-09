9d864d01d19e2d1817151e568bdedf59bee5b405
afonsocord <uc2021225966@student.uc.pt>
2024-12-09 11:35:28 +0000

    Mudanças nos models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,26 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
-class Note(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
-
-    def __str__(self):
-        return self.title
-    
-
 class emprestimo(models.Model):
+    categories = (("CHAB","Crédito Habitacional"),
+                  ("CAUT","Crédito Automotivo"),
+                  ("CEST","Crédito Estudantil"),
+                  ("CPES"),"Crédito Pessoal")
     valor = models.PositiveIntegerField()
     duracao = models.PositiveIntegerField()
     salario = models.PositiveIntegerField()
     profissao = models.CharField(max_length=100)
-    estado = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
+    documentos = models.CharField(upload_to="img")
+    tiposempr = models.CharField(choices=categories,blank= True)
     person = models.ForeignKey(User, on_delete=models.CASCADE, related_name="emprestimo")
 
 
     def __str__(self):
-        return self.estado
+        return self.person
