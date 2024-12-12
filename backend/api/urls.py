eed347d807b4d26a2286b8dbb535d14ed6de460c
afonsocord <uc2021225966@student.uc.pt>
2024-12-12 18:06:15 +0000

    12/12

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,13 +1,8 @@
+from django.urls import path,include
 from django.contrib import admin
-from django.urls import path, include
-from api.views import CreateUserView, api_login,emprestimo, loan_simulator
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from . import views
 
+urlpatterns = [path('admin/', admin.site.urls),
+               path("",include("api.urls"))
+]
 
-
-# Os paths vão permitir irmos para uma pagina/chamar uma função/fazer alguma operação
-urlpatterns = [
-    path("emprestimo/",emprestimo.as_view(), name= "emprestimo"),
-    path("",emprestimo.as_view(),name="default"),
-    path('api_login/', api_login, name='api_login'),
-    path('loan_simulator/', loan_simulator, name="Simulacao")]
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path,include
 from django.contrib import admin
-from . import views
+from django.urls import path, include
+from api.views import CreateUserView, api_login,emprestimo, loan_simulator
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-urlpatterns = [path('admin/', admin.site.urls),
-               path("",include("api.urls"))
-]
+
+
+# Os paths vão permitir irmos para uma pagina/chamar uma função/fazer alguma operação
+urlpatterns = [
+    path("emprestimo/",emprestimo.as_view(), name= "emprestimo"),
+    path("",emprestimo.as_view(),name="default"),
+    path('api_login/', api_login, name='api_login'),
+    path('loan_simulator/', loan_simulator, name="Simulacao")]
